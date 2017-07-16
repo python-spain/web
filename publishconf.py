7132cbb770a0d01fb9f65a2dc1cc8c98524ea521
Yamila Moreno <yamila.ms@gmail.com>
2017-07-16 22:23:09 +0200

    Fix urls

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = 'https://python-spain.github.io/web'
-RELATIVE_URLS = False
+#RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
