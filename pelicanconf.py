dfc3e6140371452a78055833926378edc2d96f84
Óscar M. Lage <info@oscarmlage.com>
2018-03-19 21:57:08 +0100

    Fixes small pep8 issue in pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -32,7 +32,7 @@ ICONS = (
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
 
 THEME = 'themes/pelican-alchemy/alchemy'
 
